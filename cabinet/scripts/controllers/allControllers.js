bd15a67dd959ce54d01207e330a10b33877979c5
dell <Wizlightdragon@gmail.com>
2018-06-29 13:23:24 +0300

    reg

--- cabinet/scripts/controllers/allControllers.js
+++ cabinet/scripts/controllers/allControllers.js
@@ -44,3 +44,95 @@ angular.module('sbAdminApp')
             });
         }
     }])
+    .controller('adminCtrl', ['$scope', '$http', '$state','$location', function ($scope, $http, $state, $location) {
+        $scope.auth_data = {};
+        $scope.loggin = function(data){
+            $http({
+                method: 'get',
+                url: basePath+'loggin',
+                params: data
+            }).then(function successCallback(response) {
+                $state.go("dashboard.test", {}, {reload: true});
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+        }
+
+        $scope.auth = function(data){
+            console.log(data);
+            $http({
+                method: 'post',
+                url: basePath+'auth',
+                data: data,
+            }).then(function successCallback(response) {
+                $state.go("dashboard", {}, {reload: true});
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+        }
+    }])
+    .controller('adminVacanciesCtrl', ['$scope', '$http', '$state','$stateParams', function ($scope, $http, $state, $stateParams) {
+        $scope.vacanciesList = function(data) {
+            $http({
+                method: 'get',
+                url: basePath + 'vacancies',
+                // params: data
+            }).then(function successCallback(response) {
+                $scope.vacancies = response.data;
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+        };
+        $scope.vacanciesList();
+
+        $scope.removeVacancy = function(id){
+            $http({
+                method: 'DELETE',
+                url: basePath+'vacancies',
+                params: {id:id},
+            }).then(function successCallback(response) {
+                $scope.vacanciesList();
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+        }
+    }])
+    .controller('adminVacancyCtrl', ['$scope', '$http', '$state','$stateParams', function ($scope, $http, $state, $stateParams) {
+        if($stateParams.id){
+            $http({
+                method: 'get',
+                url: basePath + 'vacancies',
+                // params: data
+            }).then(function successCallback(response) {
+                $scope.vacancies = response.data;
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+        }else{
+            alert(2);
+        }
+        $scope.vacanciesList = function(data) {
+            $http({
+                method: 'get',
+                url: basePath + 'vacancies',
+                // params: data
+            }).then(function successCallback(response) {
+                $scope.vacancies = response.data;
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+        };
+        $scope.vacanciesList();
+
+        $scope.removeVacancy = function(id){
+            $http({
+                method: 'DELETE',
+                url: basePath+'vacancies',
+                params: {id:id},
+            }).then(function successCallback(response) {
+                $scope.vacanciesList();
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+        }
+    }])
